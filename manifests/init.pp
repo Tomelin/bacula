4ac3bcd6fea4bf32f1c54f089ed301d56dd93adc
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-11 22:38:12 -0200

    $is_director ability

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,32 +36,30 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class bacula (
-  
-          $bacula_dir_package = $::bacula::params::bacula_dir_package,
-          $bacula_dir_service = $::bacula::params::bacula_dir_service,
-          $bacula_sd_package = $::bacula::params::bacula_sd_package,
-          $bacula_fd_package = $::bacula::params::bacula_fd_package,
-          $bacula_fd_service = $::bacula::params::bacula_fd_service,
-          $bconsole_package = $::bacula::params::bconsole_package,
-          $dirconf = $::bacula::params::dirconf,
-          $fdport  = $::bacula::params::fdport,
-          $workingDirectory = $::bacula::params::workingDirectory,
-          $pidDirectory = $::bacula::params::pidDirectory,
-          $maximumConcurrentJobs = $::bacula::params::maximumConcurrentJobs,
-          $typebackup = $::bacula::params::typebackup,
-          $dirBackupFile = $::bacula::params::dirBackupFile,
-          $is_client  = $::bacula::params::is_client,
-          $is_storage  = $::bacula::params::is_storage,
-          $is_director  = $::bacula::params::is_director,
-          $is_console  = $::bacula::params::is_console,
-          $password_fd = $::bacula::params::password_fd,
-          $bacula_dir = $::bacula::params::bacula_dir,
-          
-) inherits bacula::params {
-
-if $is_client == true {
-  class { 'bacula::client': }
- }
+  $bacula_dir_package    = $::bacula::params::bacula_dir_package,
+  $bacula_dir_service    = $::bacula::params::bacula_dir_service,
+  $bacula_sd_package     = $::bacula::params::bacula_sd_package,
+  $bacula_fd_package     = $::bacula::params::bacula_fd_package,
+  $bacula_fd_service     = $::bacula::params::bacula_fd_service,
+  $bconsole_package      = $::bacula::params::bconsole_package,
+  $dirconf               = $::bacula::params::dirconf,
+  $fdport                = $::bacula::params::fdport,
+  $workingDirectory      = $::bacula::params::workingDirectory,
+  $pidDirectory          = $::bacula::params::pidDirectory,
+  $maximumConcurrentJobs = $::bacula::params::maximumConcurrentJobs,
+  $typebackup            = $::bacula::params::typebackup,
+  $dirBackupFile         = $::bacula::params::dirBackupFile,
+  $is_client             = $::bacula::params::is_client,
+  $is_storage            = $::bacula::params::is_storage,
+  $is_director           = $::bacula::params::is_director,
+  $is_console            = $::bacula::params::is_console,
+  $password_fd           = $::bacula::params::password_fd,
+  $bacula_dir            = $::bacula::params::bacula_dir,) inherits bacula::params {
+  if $is_client == true {
+    class { 'bacula::client': }
   }
-
+    if $is_director == true {
+    class { 'bacula::director': }
+  }
+}
 
