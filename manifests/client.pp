a357a3eee91713cc2e4422498f88615c0059e0d7
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-13 20:53:49 -0200

    .

--- manifests/client.pp
+++ manifests/client.pp
@@ -39,24 +39,17 @@ class bacula::client (
   
 
   notify { "$dirBaculaTMP/baculaSendConfClient.sh": }
-  /**
-    file { "$dirBaculaTMP/baculaSendConfClient.sh":
-    ensure  => 'file', 
-=======
+  
     file { "$dirBaculaTMP/baculaSendConfClient.sh":
     ensure  => 'file',
->>>>>>> branch 'master' of https://github.com/Tomelin/tomelin-bacula.git
     owner   => 'bacula',
     group   => 'bacula',
     mode => '0755',
-<<<<<<< HEAD
     #content => template('bacula/scripts/baculaSendConfClient.sh.erb'),
-=======
     content => template('bacula/scripts/baculaSendConfClient.sh.erb'),
->>>>>>> branch 'master' of https://github.com/Tomelin/tomelin-bacula.git
     require => File["$dirBaculaTMP"],
   }
-   */
+   
   
 
   exec { 'SendClientConf':
