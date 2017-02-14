0c6a3bf8903f267c5c79021dcc4fcb7eb483c738
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-13 23:49:29 -0200

    .

--- manifests/director/pool.pp
+++ manifests/director/pool.pp
@@ -33,5 +33,19 @@ class bacula::director::pool (
     group   => 'bacula',
     content => template('bacula/director/pool_conf.erb')
   }
+  
+  $database_profile_array = [cpu,sysctl]
+$database_profile_hash =
+{
+  cpu   => {
+    governor => ondemand,
+    energy_perf_bias => powersave,
+  },
+  sysctl => {
+    kernel_sched_min_granularity_ns => 10000000,
+    kernel_sched_wakeup_granularity_ns => 15000000,
+    kernel_msgmnb => 6,
+  }
+}
 
 }
