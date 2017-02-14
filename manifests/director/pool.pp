ba023e31c92de77617301ff9812720429ee6bfe0
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-13 23:55:07 -0200

    .

--- manifests/director/pool.pp
+++ manifests/director/pool.pp
@@ -1,5 +1,6 @@
 class bacula::director::pool (
-  $pools = [{
+  $pools = [
+    {
       name               => 'PoolDiario',
       poolType           => 'Backup',
       recycle            => 'yes',
@@ -19,13 +20,9 @@ class bacula::director::pool (
       maximumVolumeBytes => '5G',
       maximumVolumes     => '10',
       labelFormat        => 'Local2'
-    }]) {
-  # $foo = [ 'one', {'second' => 'two', 'third' => 'three'} ]
-  # notify{ $foo[1]['third']: }
-  # notice( $foo[1]['third'] )
+    }
+    ]) {
   $teste = $pools[0]['name']
-
-  notify { $pools[0]['name']: }
 
   file { "/etc/bacula/pool/pool_$teste.conf":
     ensure  => 'file',
@@ -33,19 +30,19 @@ class bacula::director::pool (
     group   => 'bacula',
     content => template('bacula/director/pool_conf.erb')
   }
-  
-  $database_profile_array = [cpu,sysctl]
-$database_profile_hash =
-{
-  cpu   => {
-    governor => ondemand,
-    energy_perf_bias => powersave,
-  },
-  sysctl => {
-    kernel_sched_min_granularity_ns => 10000000,
-    kernel_sched_wakeup_granularity_ns => 15000000,
-    kernel_msgmnb => 6,
+
+  $database_profile_array = [cpu, sysctl]
+  $database_profile_hash = {
+    cpu              => {
+      governor         => ondemand,
+      energy_perf_bias => powersave,
+    }
+    ,
+    sysctl           => {
+      kernel_sched_min_granularity_ns    => 10000000,
+      kernel_sched_wakeup_granularity_ns => 15000000,
+      kernel_msgmnb => 6,
+    }
   }
-}
 
 }
