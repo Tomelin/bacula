4dec2b0022fbb7e93a969c45e1bf7749c89da936
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-13 23:28:21 -0200

    .

--- manifests/director/pool.pp
+++ manifests/director/pool.pp
@@ -1,44 +1,21 @@
 class bacula::director::pool (
-  $pools = [
-    {
-      name => 'PoolDiario'
+  $pools = [{
+      name               => 'PoolDiario',
+      poolType           => 'Backup',
+      recycle            => 'yes',
+      autoPrune          => 'yes',
+      volumeRetention    => '30 days',
+      maximumVolumeBytes => '1G',
+      maximumVolumes     => '100',
+      labelFormat        => 'Local-'
     }
-    ,
-    {
-      poolType => 'Backup'
-    }
-    ,
-    {
-      recycle => 'yes'
-    }
-    ,
-    {
-      autoPrune => 'yes'
-    }
-    ,
-    {
-      volumeRetention => '30 days'
-    }
-    ,
-    {
-      maximumVolumeBytes => '1G'
-    }
-    ,
-    {
-      maximumVolumes => '100'
-    }
-    ,
-    {
-      labelFormat => 'Local-'
-    }
-    ,
-    ]) {
-      
-     # $foo = [ 'one', {'second' => 'two', 'third' => 'three'} ]
-     # notify{ $foo[1]['third']: }
-      #notice( $foo[1]['third'] )
-      $teste = $pools[0]['name']
-      notify { $pools[0]['name']: }
+    ,]) {
+  # $foo = [ 'one', {'second' => 'two', 'third' => 'three'} ]
+  # notify{ $foo[1]['third']: }
+  # notice( $foo[1]['third'] )
+  $teste = $pools[0]['name']
+
+  notify { $pools[0]['name']: }
 
   file { "/etc/bacula/pool/pool_$teste.conf":
     ensure  => 'file',
