7f170f5c8500b9bc976c7961737eea2490a0c51a
root <root@puppet.tomelin.eti.br>
2017-02-21 19:31:02 -0300

    Insert CHANGELOG

---
+++ pkg/tomelin-bacula-0.0.3/lib/facter/passwordClient.rb
@@ -0,0 +1,5 @@
+Facter.add('passwordclient') do
+  setcode do
+    Facter::Core::Execution.exec('/usr/bin/facter ipaddress | sha256sum | base64 | head -c 30')
+  end
+end
---
+++ pkg/tomelin-bacula-0.0.3/manifests/director/pool.pp
@@ -0,0 +1,62 @@
+class bacula::director::pool (
+  $pools = [
+    {
+      name               => 'PoolDiario3',
+      poolType           => 'Backup',
+      recycle            => 'yes',
+      autoPrune          => 'yes',
+      volumeRetention    => '30 days',
+      maximumVolumeBytes => '1G',
+      maximumVolumes     => '100',
+      labelFormat        => 'Local-'
+    }
+    ,
+    {
+      name               => 'PoolDiario4',
+      poolType           => 'Backup',
+      recycle            => 'yes',
+      autoPrune          => 'yes',
+      volumeRetention    => '30 days',
+      maximumVolumeBytes => '1G',
+      maximumVolumes     => '100',
+      labelFormat        => 'Local-'
+    }
+
+    ]) {
+      
+      /**
+  $pools.each |$name, $pool| {
+    notify { $pool: }
+#    file { "/etc/bacula/pool/pool_$pool['name'].conf":
+#      ensure  => 'file',
+#      owner   => 'bacula',
+#      group   => 'bacula',
+#      content => template('bacula/director/pool_conf.erb')
+#    }
+  }
+   */
+  
+  $teste = $pools[0]['name']
+
+  file { "/etc/bacula/pool/pool_$teste.conf":
+    ensure  => 'file',
+    owner   => 'bacula',
+    group   => 'bacula',
+    content => template('bacula/director/pool_conf.erb')
+  }
+
+  $database_profile_array = [cpu, sysctl]
+  $database_profile_hash = {
+    cpu              => {
+      governor         => ondemand,
+      energy_perf_bias => powersave,
+    }
+    ,
+    sysctl           => {
+      kernel_sched_min_granularity_ns    => 10000000,
+      kernel_sched_wakeup_granularity_ns => 15000000,
+      kernel_msgmnb => 6,
+    }
+  }
+
+}
