fd70e9c3e6e2fdba61fa095ee841e841867396f3
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-21 20:08:27 -0300

    ...

--- lib/facter/passwordClient.rb
+++ lib/facter/passwordClient.rb
@@ -1,5 +1,9 @@
 Facter.add('passwordclient') do
   setcode do
-    Facter::Core::Execution.exec('facter ipaddress | sha256sum | base64 | head -c 30')
+  	if File.exist? '/usr/bin/facter'
+    	Facter::Core::Execution.exec('/usr/bin/facter ipaddress | sha256sum | base64 | head -c 30')
+    else
+    	Facter::Core::Execution.exec('/opt/puppetlabs/bin/facter ipaddress | sha256sum | base64 | head -c 30')
+    end
   end
 end
