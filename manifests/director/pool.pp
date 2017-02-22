9f7d96915ae134f9276d6b2d260aef426cd41dd0
rafael.tomelin <rafael.tomelin@gmail.com>
2017-02-21 22:31:07 -0300

    remove conf pool template

--- manifests/director/pool.pp
+++ manifests/director/pool.pp
@@ -62,5 +62,21 @@ class bacula::director::pool (
       kernel_msgmnb => 6,
     }
   }
+  
+  #tenmplate
+  /**
+<% pools.each do |pool| -%>
+Pool {
+  Name = "<%= pool['name'] %>"
+  Pool Type = "<%= pool['poolType'] %>"
+  Recycle = <%= pool['recycle'] %>
+  AutoPrune = <%= pool['autoPrune'] %>
+  Volume Retention = <%= pool['volumeRetention'] %>
+  Maximum Volume Bytes = <%= pool['maximumVolumeBytes'] %> 
+  Maximum Volumes = <%= pool['maximumVolumes'] %>
+  Label Format = "<%= pool['labelFormat'] %>"
+}
+<% end -%>
+**/
 
 }
