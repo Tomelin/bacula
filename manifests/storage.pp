8949979824d5ad224fc10a3d08e865dc5012d68d
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-02-28 02:41:43 -0300

    rtgrt

--- manifests/storage.pp
+++ manifests/storage.pp
@@ -9,13 +9,17 @@ class bacula::storage (
   $maximumConcurrentJobs = $::bacula::maximumConcurrentJobs,
   $db_package            = $::bacula::db_package,
   $bacula_dir            = $::bacula::bacula_dir,
+  
+  $dirBaculaTMP          = $::bacula::dirBaculaTMP,
   $db_id                 = $::bacula::db_id,) {
 
 
+#Package bacula-sd
   package { $bacula_sd_package:
     ensure => 'present',
   }
 
+#workdir
   file { "$workingDirectory":
     ensure  => directory,
     recurse => true,
@@ -48,6 +52,42 @@ class bacula::storage (
     content => template('bacula/bacula-sd.conf.erb'),
     require => Package[$bacula_sd_package],
     notify => Service[$bacula_sd_service],
+  }
+
+#File temporary para enviar via ftp para o server 
+  file { "$dirBaculaTMP":
+    ensure => 'directory',
+    owner  => 'bacula',
+    group  => 'bacula',
+  }
+  
+    file { "$dirBaculaTMP/${::hostname}.conf":
+    ensure  => 'file',
+    owner   => 'bacula',
+    group   => 'bacula',
+    content => template('bacula/director/storage_conf.erb'),
+    require => File["$dirBaculaTMP"],
+  }
+
+  file { "$dirBaculaTMP/baculaSendConf.sh":
+    ensure  => 'file',
+    owner   => 'bacula',
+    group   => 'bacula',
+    mode    => '0755',
+    content => template('bacula/scripts/baculaSendConf.sh.erb'),
+    require => File["$dirBaculaTMP"],
+    notify  => Exec['SendClientConf'],
+  }
+  
+    package { 'ftp':
+    ensure => 'present',
+  }
+  
+  exec { 'SendConf':
+    path        => ['/usr/bin', '/usr/sbin'],
+    command     => "$dirBaculaTMP/baculaSendConf.sh",
+    refreshonly => true,
+    require => Package['ftp'],
   }
 
 }
