1aee7d224d9784508a25a4c1bae0d91c0fdb9e71
root <root@testbacula.tomelin.eti.br>
2017-03-07 21:30:39 -0300

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,13 @@ Spec::Runner.configure do |config|
     config.mock_with :mocha
 end
 
+
+RSpec.configure do |c|
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
+
 # We need this because the RAL uses 'should' as a method.  This
 # allows us the same behaviour but with a different method name.
 class Object
