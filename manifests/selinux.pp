04557bc13ddf3d80d3fe57fbacd799e4037ac050
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-03-10 17:51:58 -0300

    teste

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -11,13 +11,57 @@ class bacula::selinux (
   $dir_bacula_tmp    = $::bacula::dir_bacula_tmp,
   $port_ftp          = '2121') {
   if $::os['selinux']['enabled'] == true {
-    
-
-    selinux::fcontext { 'set-mysql-log-context':
+    selinux::fcontext { 'bacula_dir_backup':
       context  => 'bacula_store_t',
       pathname => '/bacula(/.*)?',
-    # pathname => '/var/bacula(/.*)?',
     }
 
-  } 
+    selinux::fcontext { 'bacula_dir_spool':
+      context  => 'bacula_spool_t',
+      pathname => "${working_directory}.*",
+    }
+    
+    selinux::fcontext { 'bacula_dir_run':
+      context  => 'bacula_var_run_t',
+      pathname => "${pid_directory}.*",
+    }
+
+  }
 }
+
+/*
+ *
+ * bacula_spool_t
+ *
+ *      /var/spool/bacula.*
+ *
+ *
+ * bacula_store_t
+ *
+ *      /bacula(/.*)?
+ *
+ *     /var/bacula(/.*)?
+ *
+ *
+ * bacula_tmp_t
+ *
+ *
+ * bacula_var_lib_t
+ *
+ *      /var/lib/bacula.*
+ *
+ *
+ * bacula_var_run_t
+ *
+ *      /var/run/bacula.*
+ *
+ *
+ * cifs_t
+ *
+ *
+ *
+ *
+ *
+ *
+ *     bacula_log_t
+ */
