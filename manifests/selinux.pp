b7ae648cd3a5dcbaa01d0c3571af8a3df17b7a79
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-03-10 17:28:11 -0300

    d

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -1,24 +1,26 @@
 class bacula::selinux (
-  $firewall         = $::bacula::firewall,
-  $is_client        = $::bacula::is_client,
-  $is_storage       = $::bacula::is_storage,
-  $is_director      = $::bacula::is_director,
-  $is_console       = $::bacula::is_console,
-  $dirconf          = $::bacula::dirconf,
+  $firewall          = $::bacula::firewall,
+  $is_client         = $::bacula::is_client,
+  $is_storage        = $::bacula::is_storage,
+  $is_director       = $::bacula::is_director,
+  $is_console        = $::bacula::is_console,
+  $dirconf           = $::bacula::dirconf,
   $working_directory = $::bacula::working_directory,
   $pid_directory     = $::bacula::pid_directory,
-  $dir_restore_file   = $::bacula::dir_restore_file,
-  $dir_bacula_tmp     = $::bacula::dir_bacula_tmp,
+  $dir_restore_file  = $::bacula::dir_restore_file,
+  $dir_bacula_tmp    = $::bacula::dir_bacula_tmp,
   $port_ftp          = '2121') {
-  
-  
   if $::os['selinux']['enabled'] == true {
-    notify {'selinux ativado': }
-  }else{
-    notify {'selinux ativado': }
+    notify { 'selinux ativado': }
+
+    selinux::fcontext { 'set-mysql-log-context':
+      context  => 'bacula_store_t',
+      pathname => '/bacula(/.*)?',
+    # pathname => '/var/bacula(/.*)?',
+    }
+
+  } else {
+    notify { 'selinux ativado': }
   }
-  
-  
-  
-  
+
 }
