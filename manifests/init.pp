45d56d7cf19ad2a39ec81b6e403aff65b5237e96
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-03-10 14:43:32 -0300

    ew

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,39 +36,39 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class bacula (
-  $bacula_dir_package    = $::bacula::params::bacula_dir_package,
-  $bacula_dir_service    = $::bacula::params::bacula_dir_service,
-  $bacula_sd_package     = $::bacula::params::bacula_sd_package,
-  $bacula_fd_package     = $::bacula::params::bacula_fd_package,
-  $bacula_fd_service     = $::bacula::params::bacula_fd_service,
-  $bacula_sd_service     = $::bacula::params::bacula_sd_service,
-  $bconsole_package      = $::bacula::params::bconsole_package,
-  $dirconf               = $::bacula::params::dirconf,
-  $fdport                = $::bacula::params::fdport,
-  $sdport                = $::bacula::params::sdport,
-  $working_directory      = $::bacula::params::working_directory,
-  $pid_directory          = $::bacula::params::pid_directory,
+  $bacula_dir_package      = $::bacula::params::bacula_dir_package,
+  $bacula_dir_service      = $::bacula::params::bacula_dir_service,
+  $bacula_sd_package       = $::bacula::params::bacula_sd_package,
+  $bacula_fd_package       = $::bacula::params::bacula_fd_package,
+  $bacula_fd_service       = $::bacula::params::bacula_fd_service,
+  $bacula_sd_service       = $::bacula::params::bacula_sd_service,
+  $bconsole_package        = $::bacula::params::bconsole_package,
+  $dirconf                 = $::bacula::params::dirconf,
+  $fdport                  = $::bacula::params::fdport,
+  $sdport                  = $::bacula::params::sdport,
+  $working_directory       = $::bacula::params::working_directory,
+  $pid_directory           = $::bacula::params::pid_directory,
   $maximum_concurrent_jobs = $::bacula::params::maximum_concurrent_jobs,
-  $typebackup            = $::bacula::params::typebackup,
+  $typebackup              = $::bacula::params::typebackup,
   $dir_backup_file         = $::bacula::params::dir_backup_file,
   $dir_restore_file        = $::bacula::params::dir_restore_file,
-  $is_client             = $::bacula::params::is_client,
-  $is_storage            = $::bacula::params::is_storage,
-  $is_director           = $::bacula::params::is_director,
-  $is_console            = $::bacula::params::is_console,
-  $password_fd           = $::bacula::params::password_fd,
-  $dirserver             = $::bacula::params::dirserver,
+  $is_client               = $::bacula::params::is_client,
+  $is_storage              = $::bacula::params::is_storage,
+  $is_director             = $::bacula::params::is_director,
+  $is_console              = $::bacula::params::is_console,
+  $password_fd             = $::bacula::params::password_fd,
+  $dirserver               = $::bacula::params::dirserver,
   $dir_bacula_tmp          = $::bacula::params::dir_bacula_tmp,
   $dir_conf_clients        = $::bacula::params::dir_conf_clients,
   $dir_conf_storage        = $::bacula::params::dir_conf_storage,
-  $port_ftp               = $::bacula::params::port_ftp,
-  $is_director_ftp        = $::bacula::params::is_director_ftp,
-  $dirport               = $::bacula::params::dirport,
-  $db_package            = $::bacula::params::db_package,
-  $db_id                 = $::bacula::params::db_id,
-  $heartbeat_interval     = $::bacula::params::heartbeat_interval,
-  $signature             = $::bacula::params::signature,
-  $firewall              = $::bacula::params::firewall,) inherits bacula::params {
+  $port_ftp                = $::bacula::params::port_ftp,
+  $is_director_ftp         = $::bacula::params::is_director_ftp,
+  $dirport                 = $::bacula::params::dirport,
+  $db_package              = $::bacula::params::db_package,
+  $db_id                   = $::bacula::params::db_id,
+  $heartbeat_interval      = $::bacula::params::heartbeat_interval,
+  $signature               = $::bacula::params::signature,
+  $firewall                = $::bacula::params::firewall,) inherits bacula::params {
   if $is_director == true {
     class { 'bacula::director': }
 
@@ -88,11 +88,11 @@ class bacula (
 
   }
 
-  class { 'bacula::firewall':
-  }
+  include firewalld
 
-  class { 'bacula::selinux':
-  }
+  class { 'bacula::firewall': }
+
+  class { 'bacula::selinux': }
 
 }
 
