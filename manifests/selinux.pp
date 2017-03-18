e967bdbffb58f2028b323e4ec3f6d52fc8138f01
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-03-18 13:53:10 -0300

    selinux

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -12,22 +12,32 @@ class bacula::selinux (
   $port_ftp          = '2121') {
   if $::os['selinux']['enabled'] == true {
     
-    
-    selinux::fcontext { 'bacula_dir_backup':
-      context  => 'bacula_store_t',
-      pathname => '/bacula(/.*)?',
-    }
-
-    selinux::fcontext { 'bacula_dir_spool':
-      context  => 'bacula_spool_t',
-      pathname => "${working_directory}.*",
+    exec{ 'context_clients':
+      command => 'semanage fcontext -a -t public_content_rw_t "/etc/bacula/clients(/.*)?"',
+       path   => '/usr/bin:/usr/sbin:/bin',
+       unless => 'ls -lZ /etc/bacula/ | grep -v public_content_rw_t',
     }
     
-    selinux::fcontext { 'bacula_dir_run':
-      context  => 'bacula_var_run_t',
-      pathname => "${pid_directory}.*",
+        exec{ 'semanage_port_ftp':
+      command => 'semanage port -a -t ftp_port_t -p tcp 2121',
+       path   => '/usr/bin:/usr/sbin:/bin',
+       onlyif => 'semanage port -l | grep ftp_port_t | grep 2121',
+    }
+    
+        exec{ 'sebool_ftp':
+      command => 'setsebool -P ftpd_full_access 1',
+       path   => '/usr/bin:/usr/sbin:/bin',
+       unless => 'getsebool ftpd_full_access | grep off',
     }
 
+#    semanage fcontext -a -t public_content_rw_t "/etc/bacula/clients(/.*)?"
+#restorecon -R -v /etc/bacula/clients
+#semanage port -a -t ftp_port_t -p tcp 2121
+#setsebool -P ftpd_full_access 1
+    
+    
+    
+ 
   }
 }
 
